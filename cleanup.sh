c1b8a685c4207079c2e96f94b7f5cd15ecd71ebb
Jack Carnes <carnesj@us.ibm.com>
2021-03-04 17:30:16 -0500

    Delete cleanup.sh

--- cleanup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-export TARGET_NAMESPACE=cp4i
-
-oc delete queuemanager gsmq -n $TARGET_NAMESPACE
-oc delete secret gettingstartedqmgrcert -n $TARGET_NAMESPACE
-oc delete route mq-traffic-mq-gettingstartedqmgr-ibm-mq-qm -n $TARGET_NAMESPACE
-oc delete configmap gettingstartedmqsc -n $TARGET_NAMESPACE
-
