0f6e900c13bc0fa7bdc6e9b48532b5c16fd1c35f
Jack Carnes <carnesj@us.ibm.com>
2021-05-21 13:11:45 -0400

    Update install.sh

--- MQonCP4I/multiinstance/deploy/install.sh
+++ MQonCP4I/multiinstance/deploy/install.sh
@@ -7,6 +7,8 @@ export QMpre=mq00
 export ROUTE=mq00chl.chl.mq.ibm.com
 export CHANNEL=mq00chl
 export SC=managed-nfs-storage
+export VERSION=9.2.2.0-r1
+#export SC=ibmc-file-gold-gid
 
 ( echo "cat <<EOF" ; cat mqmultiinstance.yaml_template ; echo EOF ) | sh > mqmultiinstance.yaml
 
