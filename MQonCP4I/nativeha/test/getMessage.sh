6d32b9485440bcb90be519f19b7edc4eae7decb9
Jack Carnes <carnesj@us.ibm.com>
2021-05-26 11:24:21 -0500

    Update getMessage.sh

--- MQonCP4I/nativeha/test/getMessage.sh
+++ MQonCP4I/nativeha/test/getMessage.sh
@@ -3,10 +3,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export MQCCDTURL="${DIR}/ccdt_generated.json"
 export MQSSLKEYR="${DIR}/key"
+export MQCHLLIB="${DIR}"
+export MQCHLTAB="${DIR}/ccdt_generated.json"
+export QMpre=mq00
+export QMname=nativeha
+export APPQ=APPQ
 
 export ROOTURL="$(oc get IngressController default -n openshift-ingress-operator -o jsonpath='{.status.domain}')"
 ( echo "cat <<EOF" ; cat ccdt_template.json ; echo EOF ) | sh > ccdt_generated.json
 
-
-echo "Starting amqsghac" nativeha
-/opt/mqm/samp/bin/amqsghac APPQ nativeha
+echo "Starting amqsghac" $QMname
+/opt/mqm/samp/bin/amqsghac $APPQ $QMname
