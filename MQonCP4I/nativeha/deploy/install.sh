a4c4574700f7faaf326448beadcaf447c909a168
Jack Carnes <carnesj@us.ibm.com>
2021-05-26 09:59:26 -0500

    Update install.sh

--- MQonCP4I/nativeha/deploy/install.sh
+++ MQonCP4I/nativeha/deploy/install.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-#export TARGET_NAMESPACE=cp4i
+#export TARGET_NAMESPACE=cp4i-mq
 export TARGET_NAMESPACE=mq00
 export VERSION=9.2.2.0-r1
 export SC=ibmc-file-gold-gid
+#export SC=managed-nfs-storage
 
 ( echo "cat <<EOF" ; cat nativeha.yaml_template ; echo EOF ) | sh > nativeha.yaml
 
