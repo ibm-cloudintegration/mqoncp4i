313c28e34d455cfb455d9b7642a943e087928aa2
Jack Carnes <carnesj@us.ibm.com>
2021-05-26 11:34:59 -0500

    Update sendPersistentMessage.sh

--- MQonCP4I/nativeha/test/sendPersistentMessage.sh
+++ MQonCP4I/nativeha/test/sendPersistentMessage.sh
@@ -3,9 +3,15 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export MQCCDTURL="${DIR}/ccdt_generated.json"
 export MQSSLKEYR="${DIR}/key"
+export MQCHLLIB="${DIR}"
+export MQCHLTAB="${DIR}/ccdt_generated.json"
+export TARGET_NAMESPACE=cp4i-mq
+export QMpre=mq00
+export QMname=nativeha
+export APPQ=APPQ
 
 export ROOTURL="$(oc get IngressController default -n openshift-ingress-operator -o jsonpath='{.status.domain}')"
 ( echo "cat <<EOF" ; cat ccdt_template.json ; echo EOF ) | sh > ccdt_generated.json
 
-echo "Starting /opt/mqm/samp/bin/amqsputc" nativeha
-/opt/mqm/samp/bin/amqsputc APPQ nativeha
+echo "Starting /opt/mqm/samp/bin/amqsputc" $QMname
+/opt/mqm/samp/bin/amqsputc $APPQ $QMname
