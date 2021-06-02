6829893d66313a7ad0c70e964b5e424b4fe52375
Jack Carnes <carnesj@us.ibm.com>
2021-06-02 15:01:16 -0400

    Update setEnv.sh

--- MQonCP4I/unicluster/test/setEnv.sh
+++ MQonCP4I/unicluster/test/setEnv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export QMpre=MQ00
+export QMpre=mq00
 export QMname=mq00a
 export APPQ=APPQ
-export MQCHLLIB='/home/ibmuser/MQonCP4I/unicluster/test'
-export MQCHLTAB='/home/ibmuser/MQonCP4I/unicluster/test/ccdt.json'
+export MQCHLLIB='/home/student/MQonCP4I/unicluster/test'
+export MQCHLTAB='/home/student/MQonCP4I/unicluster/test/ccdt.json'
 export MQAPPLNAME='MY.GETTER.APP'
-export MQCCDTURL='/home/ibmuser/MQonCP4I/unicluster/test/ccdt.json'
-export MQSSLKEYR='/home/ibmuser/MQonCP4I/unicluster/test/key'
+export MQCCDTURL='/home/student/MQonCP4I/unicluster/test/ccdt.json'
+export MQSSLKEYR='/home/student/MQonCP4I/unicluster/test/key'
 export MQSERVER='MQ00CHLA/TCP/mq00a-ibm-mq-qm-mq.dteroks-1000006dxs-z08br-8946bbc006b7c6eb0829d088919818bb-0000.us-east.containers.appdomain.cloud(443)'
 
 echo "Starting runmqsc" $QMname
