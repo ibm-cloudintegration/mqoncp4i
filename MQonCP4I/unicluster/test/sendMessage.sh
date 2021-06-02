9456ad3b468a197edd65c693165be142f9d080ec
Jack Carnes <carnesj@us.ibm.com>
2021-06-02 07:43:30 -0400

    Update sendMessage.sh

--- MQonCP4I/unicluster/test/sendMessage.sh
+++ MQonCP4I/unicluster/test/sendMessage.sh
@@ -3,11 +3,11 @@
 export QMpre=MQ00
 export QMname=mq00a
 export APPQ=APPQ
-export MQCHLLIB='/home/ibmuser/MQonCP4I/unicluster/test'
-export MQCHLTAB='/home/ibmuser/MQonCP4I/unicluster/test/ccdt.json'
+export MQCHLLIB='/home/student/MQonCP4I/unicluster/test'
+export MQCHLTAB='/home/student/MQonCP4I/unicluster/test/ccdt.json'
 export MQAPPLNAME='MY.PUTTER.APP'
-export MQCCDTURL='/home/ibmuser/MQonCP4I/unicluster/test/ccdt.json'
-export MQSSLKEYR='/home/ibmuser/MQonCP4I/unicluster/test/key'
+export MQCCDTURL='/home/student/MQonCP4I/unicluster/test/ccdt.json'
+export MQSSLKEYR='/home/student/MQonCP4I/unicluster/test/key'
 
 echo "Starting amqsphac" $QMname
 /opt/mqm/samp/bin/amqsphac $APPQ $QMname
