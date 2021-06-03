c7a05a0f79fb0355d5144068a0bb48aadfc9ed96
Jack Carnes <carnesj@us.ibm.com>
2021-06-03 07:42:40 -0400

    Update cleanup.sh

--- MQonCP4I/nativeha/deploy/cleanup.sh
+++ MQonCP4I/nativeha/deploy/cleanup.sh
@@ -3,8 +3,9 @@
 #mq00 reserved for instructor
 #export TARGET_NAMESPACE=cp4i-mq
 export TARGET_NAMESPACE=cp4i-mq
+export QMname=mq00ha
 
-oc delete queuemanager nativeha -n $TARGET_NAMESPACE
+oc delete queuemanager $QMname -n $TARGET_NAMESPACE
 oc delete secret nativehacert -n $TARGET_NAMESPACE
 oc delete configmap nativehamqsc -n $TARGET_NAMESPACE
 oc delete pvc data-nativeha-ibm-mq-0 -n $TARGET_NAMESPACE
