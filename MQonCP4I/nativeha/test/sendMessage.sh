021387fe569a805257877c0fc4fea912e7e4a3ab
Jack Carnes <carnesj@us.ibm.com>
2021-06-03 09:29:39 -0400

    Update sendMessage.sh

--- MQonCP4I/nativeha/test/sendMessage.sh
+++ MQonCP4I/nativeha/test/sendMessage.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+#mq00 reserved for instructor
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export MQCCDTURL="${DIR}/ccdt_generated.json"
 export MQSSLKEYR="${DIR}/key"
 export MQCHLLIB="${DIR}"
 export MQCHLTAB="${DIR}/ccdt_generated.json"
+export TARGET_NAMESPACE=cp4i-mq
 export QMpre=mq00
-export QMname=nativeha
+export QMname=mq00ha
 export APPQ=APPQ
 export ROOTURL="$(oc get IngressController default -n openshift-ingress-operator -o jsonpath='{.status.domain}')"
 ( echo "cat <<EOF" ; cat ccdt_template.json ; echo EOF ) | sh > ccdt_generated.json
