292ea9f6ccfb1ee59b2e87ffba42d9a632115e3b
Jack Carnes <carnesj@us.ibm.com>
2021-06-10 12:52:36 -0500

    Add files via upload

--- MQonCP4I/nativeha/deploy/install.sh
+++ MQonCP4I/nativeha/deploy/install.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 #mq00 reserved for instructor
-#export TARGET_NAMESPACE=cp4i-mq
+#export TARGET_NAMESPACE=mq00
 export TARGET_NAMESPACE=cp4i-mq
-export QMname=mq00ha
+export QMname=mq99ha
 export VERSION=9.2.2.0-r1
-#export SC=ibmc-file-gold-gid
-export SC=managed-nfs-storage
+#export SC=managed-nfs-storage
+export SC=ibmc-file-gold-gid
 
 ( echo "cat <<EOF" ; cat nativeha.yaml_template ; echo EOF ) | sh > nativeha.yaml
 
