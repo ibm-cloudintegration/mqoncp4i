52d205ff37e6dc0729da4e7c9de9ba8a2ade6af8
Jack Carnes <carnesj@us.ibm.com>
2021-06-29 10:23:05 -0500

    Update install.sh

--- MQonCP4I/nativeha/deploy/install.sh
+++ MQonCP4I/nativeha/deploy/install.sh
@@ -3,9 +3,9 @@
 #mq00 reserved for instructor
 #export TARGET_NAMESPACE=mq00
 export TARGET_NAMESPACE=cp4i
-export QMname=mq22ha
-export CHANNEL=mq22hachl
-export CHLCAPS=MQ22HACHL
+export QMname=mq00ha
+export CHANNEL=mq00hachl
+export CHLCAPS=MQ00HACHL
 export VERSION=9.2.2.0-r1
 #export SC=managed-nfs-storage
 export SC=ibmc-file-gold-gid
