fcb81a55e86c0e8480757edffab46df7421bbced
Jack Carnes <carnesj@us.ibm.com>
2021-09-22 09:37:44 -0500

    Add files via upload

---
+++ MQonCP4I/streamq/deploy/cleanup.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#mq99 reserved for instructor
+export TARGET_NAMESPACE=cp4i-mq	
+export QMname=mq99strm
+
+oc delete queuemanager $QMname -n $TARGET_NAMESPACE
+oc delete secret streamqqmgrcert -n $TARGET_NAMESPACE
+oc delete configmap streamqmqsc -n $TARGET_NAMESPACE
+oc delete route mq-traffic-mq-$QMname-ibm-mq-qm -n $TARGET_NAMESPACE
+oc delete pvc data-$QMname-ibm-mq-0 -n $TARGET_NAMESPACE
