a934f3d5a7bb6c37deaa5901dbb2f14ecf2c8dc8
Jack Carnes <carnesj@us.ibm.com>
2021-10-05 13:16:09 -0500

    Update install.sh

--- MQonCP4I/streamq/deploy/install.sh
+++ MQonCP4I/streamq/deploy/install.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
-#mq99 reserved for instructor
+#Use storage class ibmc-file-gold-gid when running on ROKS clusters
+#Use storage class managed-nfs-storage when running on CoC PoT clusters
+#mq00 reserved for instructor
 export TARGET_NAMESPACE=cp4i-mq
-export QMname=mq99strm
-export QMpre=mq99
-export ROUTE=mq99strmchl.chl.mq.ibm.com
-export CHANNEL=mq99strmchl
-export CHLCAPS=mq99STRMCHL
+export QMname=mq00strm
+export QMpre=mq00
+export ROUTE=mq00strmchl.chl.mq.ibm.com
+export CHANNEL=mq00strmchl
+export CHLCAPS=mq00STRMCHL
 export VERSION=9.2.3.0-r1
 export SC=ibmc-file-gold-gid
 #export SC=managed-nfs-storage
