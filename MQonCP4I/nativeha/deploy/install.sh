9baea3d8809cdc657d17f34fc4ddbeb3ba7df63b
Jack Carnes <carnesj@us.ibm.com>
2021-10-07 13:01:34 -0400

    Update install.sh

--- MQonCP4I/nativeha/deploy/install.sh
+++ MQonCP4I/nativeha/deploy/install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+#Use storage class ibmc-file-gold-gid when running on ROKS clusters
+#Use storage class managed-nfs-storage when running on CoC PoT clusters
 #mq00 reserved for instructor
 #export TARGET_NAMESPACE=mq00
 export TARGET_NAMESPACE=cp4i
@@ -7,8 +9,8 @@ export QMname=mq00ha
 export CHANNEL=mq00hachl
 export CHLCAPS=MQ00HACHL
 export VERSION=9.2.3.0-r1
-#export SC=managed-nfs-storage
-export SC=ibmc-file-gold-gid
+export SC=managed-nfs-storage
+#export SC=ibmc-file-gold-gid
 
 ( echo "cat <<EOF" ; cat nativeha.yaml_template ; echo EOF ) | sh > nativeha.yaml
 
