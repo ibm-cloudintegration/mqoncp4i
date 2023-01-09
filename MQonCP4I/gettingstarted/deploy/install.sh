639a002b2da091739ee388aef09a0c1aa1239252
Jack Carnes <carnesj@us.ibm.com>
2023-01-09 07:54:14 -0500

    Update install.sh

--- MQonCP4I/gettingstarted/deploy/install.sh
+++ MQonCP4I/gettingstarted/deploy/install.sh
@@ -2,6 +2,7 @@
 
 export TARGET_NAMESPACE=cp4i-mq
 export VERSION=9.3.0.1-r4
+export LICENSE=L-RJON-CD3JKX
 
 ( echo "cat <<EOF" ; cat gettingstartedqmgr.yaml_template ; echo EOF ) | sh > gettingstartedqmgr.yaml
 
