d4be4e78c39182acdb387baa13abffced7f55906
Jack Carnes <carnesj@us.ibm.com>
2023-05-23 14:36:46 -0400

    Update install.sh for storage class and version

--- MQonCP4I/nativeha/deploy/install.sh
+++ MQonCP4I/nativeha/deploy/install.sh
@@ -7,10 +7,12 @@ export TARGET_NAMESPACE=cp4i-mq
 export QMname=mq00ha
 export CHANNEL=mq00hachl
 export CHLCAPS=MQ00HACHL
-export VERSION=9.3.0.1-r4
+#export VERSION=9.3.0.1-r4
+export VERSION=9.3.0.1-r2
 export LICENSE=L-RJON-CD3JKX
-export SC=managed-nfs-storage
+#export SC=managed-nfs-storage
 #export SC=ibmc-file-gold-gid
+#export SC=ocs-storagecluster-cephfs
 
 ( echo "cat <<EOF" ; cat nativeha.yaml_template ; echo EOF ) | sh > nativeha.yaml
 
