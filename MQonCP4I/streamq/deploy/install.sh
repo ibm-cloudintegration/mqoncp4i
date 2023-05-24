1055ab1aa2ba1d538ae94007c108c4773f5da9b4
Jack Carnes <carnesj@us.ibm.com>
2023-05-24 11:23:05 -0400

    Update install.sh for storage class and version

--- MQonCP4I/streamq/deploy/install.sh
+++ MQonCP4I/streamq/deploy/install.sh
@@ -9,10 +9,12 @@ export QMpre=mq00
 export ROUTE=mq00strmchl.chl.mq.ibm.com
 export CHANNEL=mq00strmchl
 export CHLCAPS=mq00STRMCHL
-export VERSION=9.3.0.1-r4
 export LICENSE=L-RJON-CD3JKX
-export SC=ibmc-file-gold-gid
+#export VERSION=9.3.0.1-r4
+export VERSION=9.3.0.1-r2
 #export SC=managed-nfs-storage
+#export SC=ibmc-file-gold-gid
+export SC=ocs-storagecluster-cephfs
 
 ( echo "cat <<EOF" ; cat streamq.yaml_template ; echo EOF ) | sh > streamq.yaml
 
